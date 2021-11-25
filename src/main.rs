f338f2118ed9111b2785143bcb0bb3398e1d1a00
banana-GK <tomsawyer404@outlook.com>
2021-11-25 16:37:23 +0800

    factor: Step 2
    A module in other file.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,4 @@
-mod something {
-    #[derive(Debug)]
-    pub struct A {
-        pub a: i32,
-    }
-
-    #[derive(Debug)]
-    pub struct B {
-        pub b: i32,
-    }
-}
+mod something;
 
 use crate::something::*;
 
